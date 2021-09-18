6ebe7480905781116169327cbd82e0e10cc93a89
kevinspasiuk <kevin_spasiuk@hotmail.com>
2021-09-18 09:08:20 -0300

    Add container class from previous exercise, get /productos return all products

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = process.env.port || 8080
+
+const Contenedor =  require("./model/contenedor.js")
+
+const productosRepository = new Contenedor("./db/productos.txt")
+
+
+app.get('/', (req, res) => {
+    res.send("Holis")
+})
 
 app.get('/productos', (req, res) => {
-  res.send("productos")
+  productosRepository.getAll()
+  .then(productos => { res.send(productos) })
 })
 
 app.get('/productoRandom', (req, res) => {
