63783df2b95a4fddd549ef98d8fefd2770736bd3
kevinspasiuk <kevin_spasiuk@hotmail.com>
2021-09-18 09:30:38 -0300

    Random product added and lint fixed

--- main.js
+++ main.js
@@ -2,26 +2,30 @@ const express = require('express')
 const app = express()
 const port = process.env.port || 8080
 
-const Contenedor =  require("./model/contenedor.js")
-
-const productosRepository = new Contenedor("./db/productos.txt")
-
+const Contenedor = require('./model/contenedor.js')
+const productosRepository = new Contenedor('./db/productos.txt')
 
 app.get('/', (req, res) => {
-    res.send("Holis")
+  res.send('Holis')
 })
 
 app.get('/productos', (req, res) => {
   productosRepository.getAll()
-  .then(productos => { res.send(productos) })
+    .then(productos => { res.send(productos) })
+    .catch(err => { console.log('Error getting productos: ', err) })
 })
 
 app.get('/productoRandom', (req, res) => {
-    res.send("productoRandom")
-  })
+  productosRepository.getAll()
+    .then(productos => {
+      const productoRandom = productos[Math.floor(Math.random()*productos.length)]
+      res.send(productoRandom)
+    })
+    .catch(err => { console.log('Error getting a random producto: ', err) })
+})
 
 const server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-server.on("error", error => console.log("Error on server: ", error))
+server.on('error', error => console.log('Error on server: ', error))
